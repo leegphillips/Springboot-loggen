4b1c9dd463ff2910d7ace612330d179c164e1b49
leegphillips <leegphillips@yahoo.co.uk>
2017-08-19 11:06:31 +0100

    Initial add

---
+++ src/main/java/com/github/leegphillips/loggen/LoggenApplication.java
@@ -0,0 +1,12 @@
+package com.github.leegphillips.loggen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggenApplication.class, args);
+	}
+}
